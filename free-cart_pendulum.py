67d9fc21d06a7a166d58bfc454c5b54ab4a84ed1
Impervguin <75371928+Impervguin@users.noreply.github.com>
2021-03-23 17:58:52 +0300

    Update free-cart_pendulum.py

--- free-cart_pendulum.py
+++ free-cart_pendulum.py
@@ -1,11 +1,12 @@
 import gym as g
+import random as r
 from Fuzzy_number_class import fuzzy
 
 STEP = 0.05
 # cart_position, pole_angle, push_direction
-RULES = [[fuzzy(-2.4, -1, 1, 2.4), fuzzy(-42, -40, -10, 0), fuzzy(-1, -0.5, -0.5, 0)],
-         [fuzzy(-2.4, -1, 1, 2.4), fuzzy(0, 10, 40, 42), fuzzy(0, 0.5, 0.5, 1)],
-         [fuzzy(-100, -99, -3, -2.4), fuzzy(-15, -10, 10, 15), fuzzy(0.5, 1, 1, 1.5)],
+RULES = [[fuzzy(*sorted([r.uniform(-2.4, 2.4) for _ in range(4)])), fuzzy(*sorted([r.randint(-42, 10) for _ in range(4)])), fuzzy(*sorted([r.uniform(-1, 0) for _ in range(4)]))],
+         [fuzzy(*sorted([r.uniform(-2.4, 2.4) for _ in range(4)])), fuzzy(*sorted([r.randint(-10, 42) for _ in range(4)])), fuzzy(*sorted([r.uniform(0, 1) for _ in range(4)]))],
+         [fuzzy(*sorted([r.uniform(-2.4, 2.4) for _ in range(4)])), fuzzy(*sorted([r.randint(-10, 42) for _ in range(4)])), fuzzy(*sorted([r.uniform(0, 1) for _ in range(4)]))],
          [fuzzy(2.4, 3, 99, 100), fuzzy(-15, -10, 10, 15), fuzzy(-0.5, 0, 0, 0.5)]]
 env = g.make("CartPole-v1")
 observation = env.reset()
